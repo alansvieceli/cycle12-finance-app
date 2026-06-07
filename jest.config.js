0bdde4b86fa4af4495feebf4c3c6784b33d34ce2
Alan Vieceli <alansvieceli@gmail.com>
2026-06-07 15:40:16 -0300

    chore: enforce 80% coverage threshold on lib/storage/types

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,18 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/?(*.)+(test).[tj]s?(x)'],
+  collectCoverageFrom: [
+    'src/lib/**/*.{ts,tsx}',
+    'src/storage/**/*.{ts,tsx}',
+    'src/types/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
